42b06f64133a856102f2ce546500ffa5a977f306
SatelierDiego <organicblue@gmail.com>
2026-05-19 21:06:33 -0300

    output: static

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
 
 export default defineConfig({
-  site: 'https://felinos.ar',
+  output: 'static',
   trailingSlash: 'ignore',
   integrations: [
     sitemap(),
@@ -14,20 +14,22 @@ export default defineConfig({
       iconDir: 'src/assets/icons/tabler/outline',
     }),
   ],
-  fonts: [{
-    provider: fontProviders.fontsource(),
-    name: "Open Sans",
-    cssVariable: "--font-open-sans",
-    styles: ["normal"],
-    weights: ["300 800"],
-  },
-  {
-    provider: fontProviders.fontsource(),
-    name: "Quicksand",
-    cssVariable: "--font-quicksand",
-    styles: ["normal"],
-    weights: ["300 700"],
-  }],
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: 'Open Sans',
+      cssVariable: '--font-open-sans',
+      styles: ['normal'],
+      weights: ['300 800'],
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: 'Quicksand',
+      cssVariable: '--font-quicksand',
+      styles: ['normal'],
+      weights: ['300 700'],
+    },
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
